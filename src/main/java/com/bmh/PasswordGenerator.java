9c015b47a4fbfa78adfdf3827fab9ebdea02af0b
bmh_gh <27549756+chronal-V@users.noreply.github.com>
2023-03-19 22:00:06 +0100

    Implement PasswordGenerator

---
+++ src/main/java/com/bmh/PasswordGenerator.java
@@ -0,0 +1,47 @@
+package com.bmh;
+
+import java.util.Random;
+
+public class PasswordGenerator {
+    public static String generate(boolean upper, boolean lower, boolean num, boolean special, short length) {
+        StringBuilder sb = new StringBuilder();
+        if (upper) {
+            sb.append(getUpperCase());
+        }
+        if (lower) {
+            sb.append(getLowerCase());
+        }
+        if (num) {
+            sb.append(getNumerical());
+        }
+        if (special) {
+            sb.append(getSpecial());
+        }
+        char[] set = sb.toString().toCharArray();
+        // Maximal password length is 128, so every other number will be adjusted
+        length = (short) (length & 128);
+        Random rng = new Random();
+        StringBuilder pw = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int rand = rng.nextInt(set.length);
+            pw.append(set[rand]);
+        }
+        return pw.toString();
+    }
+    private static String getLowerCase() {
+        return "abcdefghijklmnopqrstuvwxyz";
+    }
+
+    private static String getUpperCase() {
+        return "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    private static String getNumerical() {
+        return "1234567890";
+    }
+
+    private static String getSpecial() {
+        return "!@#$%^&*()-_=+[{]};:,<.>/?\\|`~\"";
+    }
+}
+
---
+++ src/test/java/com/bmh/PasswordGeneratorTest.java
@@ -0,0 +1,25 @@
+package com.bmh;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class PasswordGeneratorTest {
+
+    @Test
+    void generate() {
+        // Setup
+        short length = (short) 128;
+        String alphabetic_pw = PasswordGenerator.generate(true, true, false, false, length);
+        String non_alphabetic_pw = PasswordGenerator.generate(false, false, true, true, length);
+        String alphanumeric_pw = PasswordGenerator.generate(true, true, true, false, length);
+        // Tests
+        assertNotEquals(alphabetic_pw, "");
+        assertNotEquals(non_alphabetic_pw, "");
+        assertNotEquals(alphanumeric_pw, "");
+        assert(alphabetic_pw.matches("[a-zA-Z]+"));
+        assert(alphabetic_pw.matches("[0-9a-zA-Z]+"));
+        assert(!non_alphabetic_pw.matches("[a-zA-Z]+"));
+    }
+}
