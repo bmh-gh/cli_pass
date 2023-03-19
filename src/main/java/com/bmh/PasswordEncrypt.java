e16e74fd0a3871509d1831932829edbef48a2f8d
bmh_gh <27549756+chronal-V@users.noreply.github.com>
2023-03-19 23:03:26 +0100

    Password encryption with master password in AES block cipher, ECB Mode

---
+++ src/main/java/com/bmh/PasswordEncrypt.java
@@ -0,0 +1,42 @@
+package com.bmh;
+
+import javax.crypto.*;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+public class PasswordEncrypt {
+    private final String algorithm = "AES/ECB/PKCS5Padding";
+    // is it safe to do it like this??
+    private final SecretKey k;
+
+    PasswordEncrypt(String user_pw) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        k = getKeyFromPassword(user_pw);
+    }
+
+    private static SecretKey getKeyFromPassword(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecureRandom sr = SecureRandom.getInstanceStrong();
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
+        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+    }
+
+    public String encrypt_password(String plain) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException {
+        Cipher c = Cipher.getInstance(algorithm);
+        c.init(Cipher.ENCRYPT_MODE, k);
+        return Base64.getEncoder().encodeToString(c.doFinal(plain.getBytes()));
+    }
+
+    public String decrypt_password(String cipher) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException {
+        Cipher c = Cipher.getInstance(algorithm);
+        c.init(Cipher.DECRYPT_MODE, k);
+        return new String(c.doFinal(Base64.getDecoder().decode(cipher)));
+    }
+}
---
+++ src/test/java/com/bmh/PasswordEncryptTest.java
@@ -0,0 +1,25 @@
+package com.bmh;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+class PasswordEncryptTest {
+
+    @Test
+    void encrypt_password() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        String pw = "simple_password";
+        String master_pw = "1234qwer";
+        PasswordEncrypt passwordEncrypt = new PasswordEncrypt(master_pw);
+        String cipherText = passwordEncrypt.encrypt_password(pw);
+        String plainText = passwordEncrypt.decrypt_password(cipherText);
+        Assertions.assertEquals(pw, plainText);
+    }
+}
