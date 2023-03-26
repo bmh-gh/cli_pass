b27aa0cd2ecbed0f7efb4c6e484908a59d21175a
bmh_gh <27549756+chronal-V@users.noreply.github.com>
2023-03-26 17:04:13 +0200

    Implement functionality to add encrypted Entries to the storage

---
+++ src/main/java/com/github/bmhgh/commands/AddPasswordCommand.java
@@ -0,0 +1,108 @@
+package com.github.bmhgh.commands;
+
+import com.github.bmhgh.models.Entry;
+import com.github.bmhgh.services.tools.EncryptionTool;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "add",
+        mixinStandardHelpOptions = true,
+        header = "Add a new password to the storage",
+        optionListHeading = "%nOptions are:%n",
+        description = "")
+public class AddPasswordCommand implements Callable<Integer> {
+
+    @Option(names = {"-f", "--file"}, defaultValue = "")
+    private Path path;
+
+    @Parameters(paramLabel = "<title>", index = "0", description = "Title for the new entry", interactive = true)
+    String entryTitle;
+    @Parameters(paramLabel = "<url>", index = "0", description = "URL to the website for the new entry", interactive = true)
+    String entryUrl;
+    @Parameters(paramLabel = "<password>", index = "0", description = "Password for the new entry", interactive = true)
+    String entryPassword;
+
+    @Override
+    public Integer call() {
+        // Check if path variable is not set -> if not, take current path. The default file name is passwords
+        if (path.toString().equals("")) {
+            path = Paths.get(".").toAbsolutePath().normalize().resolve("passwords");
+        }
+
+        char[] password;
+
+        if (System.console() == null) {
+            Scanner scanner = new Scanner(System.in);
+            password = scanner.nextLine().toCharArray();
+            entryTitle = scanner.nextLine();
+            entryUrl = scanner.nextLine();
+            entryPassword = scanner.nextLine();
+        } else {
+            password = System.console().readPassword("Password to access the data storage: ");
+            if (entryTitle == null) {
+                entryTitle = System.console().readLine("Title: ");
+            }
+            if (entryUrl == null) {
+                entryUrl = System.console().readLine("URL: ");
+            }
+            if (entryPassword == null) {
+                entryPassword = Arrays.toString(System.console().readPassword("Password: "));
+            }
+        }
+        Entry newEntry = new Entry(entryTitle, entryUrl, entryPassword);
+        try {
+            appendEntry(newEntry, path, password);
+        } catch (Exception e) {
+            // there was an error:
+            return 1;
+        }
+        // everything went smoothly:
+        return 0;
+    }
+
+    private void appendEntry(Entry entry, Path path, char[] password) {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream out = new ObjectOutputStream(bos)) {
+            out.writeObject(entry);
+            out.flush();
+            String serializedEntry = Arrays.toString(bos.toByteArray());
+
+            // Encrypt Entry
+            String encryptedEntry = EncryptionTool.encryptData(
+                    serializedEntry,
+                    EncryptionTool.getKeyFromPassword(password)
+            );
+            // Add the Entry to the Json
+            try (FileReader reader = new FileReader(path.toFile())) {
+                JsonObject obj = JsonParser.parseReader(reader)
+                        .getAsJsonObject();
+                reader.close();
+                obj.get("passwords")
+                        .getAsJsonArray()
+                        .add(encryptedEntry);
+                // Write the manipulated Json to the file
+                try (FileWriter writer = new FileWriter(path.toFile())) {
+                    writer.write(obj.toString());
+                }
+            }
+        } catch (IOException | IllegalBlockSizeException | NoSuchPaddingException | BadPaddingException |
+                 NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/github/bmhgh/commands/AddPasswordCommandTest.java
@@ -0,0 +1,46 @@
+package com.github.bmhgh.commands;
+
+import com.github.bmhgh.CliApp;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddPasswordCommandTest {
+
+    CommandLine cli = new CommandLine(new CliApp());
+
+    @BeforeEach
+    void setup() {
+        String[] args = {"new", "--password=mypassword", "testfile"};
+        cli.execute(args);
+    }
+
+    @AfterEach
+    void end() throws IOException {
+        Files.deleteIfExists(Paths.get("testfile"));
+    }
+
+    @Test
+    void addPassword() throws IOException {
+        // Create input to simulate
+        String simulatedInput = "MyTitle\nMyUrl\nMyPassword\nsecretpassword\n";
+
+        // Set the input stream to a ByteArrayInputStream with the simulated input
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+
+        String[] args = {"add", "--file=testfile"};
+        int exitCode = cli.execute(args);
+
+        String content = Files.readString(Paths.get("testfile"));
+        assertNotNull(content);
+        assertEquals(0, exitCode);
+    }
+}
