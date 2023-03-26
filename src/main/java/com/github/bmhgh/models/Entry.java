632a4c98a0b3a8983650b86a221ebac6033f0d07
bmh_gh <27549756+chronal-V@users.noreply.github.com>
2023-03-26 19:58:57 +0200

    Add serialization & deserialization

--- src/main/java/com/github/bmhgh/models/Entry.java
+++ src/main/java/com/github/bmhgh/models/Entry.java
@@ -1,7 +1,7 @@
 package com.github.bmhgh.models;
 
-import java.io.Serial;
-import java.io.Serializable;
+import java.io.*;
+import java.util.Base64;
 
 public class Entry implements Serializable {
     @Serial
@@ -37,5 +37,35 @@ public class Entry implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public static String serialize(Entry entry) throws IOException {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream out = new ObjectOutputStream(bos)) {
+            out.writeObject(entry);
+            out.flush();
+            return Base64.getEncoder().encodeToString(bos.toByteArray());
+        } catch (IOException e) {
+            throw new IOException(e);
+        }
+    }
+
+    public static Entry deserialize(String entry) {
+        byte[] decodedInput = Base64.getDecoder().decode(entry);
+        ByteArrayInputStream bis = new ByteArrayInputStream(decodedInput);
+        try (ObjectInput in = new ObjectInputStream(bis)) {
+            Object o = in.readObject();
+            if (o instanceof Entry) {
+                return (Entry) o;
+            }
+            throw new Exception("This Entry is not valid");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s|%s|%s]", getTitle(), getUrl(), getPassword());
     }
 }
