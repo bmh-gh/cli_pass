53dc2007176bd1a390c25c71ddb8cf6ad2479c3c
bmh_gh <27549756+chronal-V@users.noreply.github.com>
2023-03-26 17:11:44 +0200

    Add subcommand `add` to the

--- src/main/java/com/github/bmhgh/CliApp.java
+++ src/main/java/com/github/bmhgh/CliApp.java
@@ -1,8 +1,13 @@
 package com.github.bmhgh;
 
+import com.github.bmhgh.commands.AddPasswordCommand;
 import com.github.bmhgh.commands.CreateFileCommand;
 import picocli.CommandLine;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "clipwm",
@@ -13,14 +18,14 @@ import java.util.concurrent.Callable;
         optionListHeading = "%nOptions are:%n",
         commandListHeading = "%nSubcommands are:%n",
         subcommands = {
-                CreateFileCommand.class
-                //add more subcommands later here
+                CreateFileCommand.class,
+                AddPasswordCommand.class,
         },
         description = "This is a simple password manager app for the command line"
 )
 public class CliApp implements Callable<Integer> {
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new CliApp()).execute("new", "-p");
+        int exitCode = new CommandLine(new CliApp()).execute(args);
         System.exit(exitCode);
     }
 
